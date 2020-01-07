7972e194d7c19175a934ae9e2ce9ee64aa2e291a
sagdish <forsi1@yahoo.com>
2020-01-07 14:58:39 +0500

    site update: 'positions' section, other tweaks

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
-import React from 'react';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
+
+// styles for this kit
+import "./assets/css/bootstrap.min.css";
+import "./assets/scss/now-ui-kit.scss";
+import "./assets/demo/demo.css";
+import "./assets/demo/nucleo-icons-page-styles.css";
+
+import Home from './pages/Home';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <h1>THIS IS ARC-REGISTRY!</h1>
+      <Home />
     </div>
   );
 }
